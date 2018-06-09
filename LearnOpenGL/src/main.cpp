bf6cf03712827daddf33313abf3e5e9b13a06422
wolfstrasz <boyan.yotov@abv.bg>
2018-06-09 16:56:13 +0100

    Created Vertex Buffer Object

--- LearnOpenGL/src/main.cpp
+++ LearnOpenGL/src/main.cpp
@@ -17,6 +17,10 @@ void processInput(GLFWwindow *window)
 
 int main(void)
 {
+
+	/************************************************************************/
+	/*							SETUP GLFW									*/
+	/************************************************************************/
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);						///> Use Versions 4.0 ++
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);						///> Use version 4.5
@@ -24,7 +28,9 @@ int main(void)
 	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);			    ///> Set profile to Forward profile (macOS)
 
 
-	// Set first window and check if created
+	/************************************************************************/
+	/*							CREATING WINDOW								*/
+	/************************************************************************/
 	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
 	if (window == nullptr)
 	{
@@ -35,22 +41,56 @@ int main(void)
 
 	// set Context to window
 	glfwMakeContextCurrent(window);
-
-	//Load OpenGL Functions
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	
+	/************************************************************************/
+	/*							LOAD OpenGL FUNCTIONS						*/
+	/************************************************************************/
+	
+	if (!gladLoadGL())
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
 	fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
-
+	
 	// Set up View Port
 	glViewport(0, 0, 800, 600);
 
 	// Set up FrameBuffer Size Callback for given window
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	// Create render loop
+
+	/************************************************************************/
+	/*							CREATING TRIANGLES							*/
+	/************************************************************************/
+
+	// First triangle:
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f,  0.5f, 0.0f
+	};
+
+	/************************************************************************/
+	/*							VERTEX BUFFER OBJECT						*/
+	/************************************************************************/
+
+	// Generate vertex buffer object VBO
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+
+	// Bind VBO to ARRAY_BUFFER => as VBO is a vertex we use ARRAY_BUFFER
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	// Copy data to buffer memory (buffer_used, size of array, array, type of draw (static/dynamic)
+	// Static = modified once and used as source many times, Dynamic = modified repeatedly and used many times
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+	/************************************************************************/
+	/*							RENDER LOOP									*/
+	/************************************************************************/
+
 	while (!glfwWindowShouldClose(window))
 	{
 		// input
