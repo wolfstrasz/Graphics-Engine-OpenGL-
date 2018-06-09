c3c75eb6df0524084017650b7f42335e2a9c3520
wolfstrasz <boyan.yotov@abv.bg>
2018-06-09 16:00:04 +0100

    Added first window

--- LearnOpenGL/src/main.cpp
+++ LearnOpenGL/src/main.cpp
@@ -1,7 +1,70 @@
-#include <GLFW/glfw3.h>
-//#include <glad/glad.h>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+#include<iostream> 
+
+// resize window and viewport
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+// Check if btn is pressed while window is on, if ESC btn is pressed window should close
+void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
 
 int main(void)
 {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);						///> Use Versions 4.0 ++
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);						///> Use version 4.5
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);		///> Set profile to Core profile
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);			    ///> Set profile to Forward profile (macOS)
+
+
+	// Set first window and check if created
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	// set Context to window
+	glfwMakeContextCurrent(window);
+
+	//Load OpenGL Functions
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+	fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
+
+	// Set up View Port
+	glViewport(0, 0, 800, 600);
+
+	// Set up FrameBuffer Size Callback for given window
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// Create render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// input
+		processInput(window);
+
+		// rendering commands:
+		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// check and call events and swap the buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	// Clean all resources
+	glfwTerminate();
 	return 0;
 }
