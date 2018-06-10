24b71173bf635d3d0c8ecb96b13b594a8da2f187
wolfstrasz <boyan.yotov@abv.bg>
2018-06-10 10:33:32 +0100

    Few changes to shaders, added second attrib to Vertex shader, binding and unbinding of buffers

--- LearnOpenGL/src/main.cpp
+++ LearnOpenGL/src/main.cpp
@@ -5,17 +5,21 @@
 // Basic Vertex Shader string
 const char *basicVertexShaderSource = "#version 450 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"   gl_Position = vec4(aPos, 1.0);\n"
+"	ourColor = aColor;\n"
 "}\0";
 
 // Basic Fragment Shader string
 const char *fragmentShaderSource = "#version 450 core\n"
 "out vec4 FragColor;\n"
+"in vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"   FragColor = vec4(ourColor,1.0f);\n"
 "}\n\0";
 
 // For debugging
@@ -37,7 +41,7 @@ void processInput(GLFWwindow *window)
 
 int main(void)
 {
-
+	
 	/************************************************************************/
 	/*							SETUP GLFW									*/
 	/************************************************************************/
@@ -79,6 +83,12 @@ int main(void)
 	// Set up FrameBuffer Size Callback for given window
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+
+	// Check number of vertex attributes available 
+	int nrAttributes;
+	glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
+	std::cout << "Maximum nr of vertex attributes supported: " << nrAttributes << std::endl;
+
 	/************************************************************************/
 	/*						SHADERS: SET && COMPILE							*/
 	/************************************************************************/
@@ -110,6 +120,10 @@ int main(void)
 		glGetShaderInfoLog(basicFragmentShader, 512, NULL, infoLog);
 		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
 	}
+
+	/************************************************************************/
+	/*						SHADERS PROGRAMS								*/
+	/************************************************************************/
 
 	// Creating Shader program
 	unsigned int shaderProgram;
@@ -148,10 +162,10 @@ int main(void)
 	unsigned int indices_size = triangle_count * 3;
 
 	float vertices[] = {
-		0.5f,  0.5f, 0.0f,   // [0] = top right 
-		0.5f, -0.5f, 0.0f,   // [1] = bottom right
-		-0.5f, -0.5f, 0.0f,  // [2] = bottom left
-		-0.5f,  0.5f, 0.0f   // [3] = top left 
+		0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f,  // [0] = top right 
+		0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,  // [1] = bottom right
+		-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // [2] = bottom left
+		-0.5f,  0.5f, 0.0f, 0.0f, 1.0f, 0.0f  // [3] = top left 
 	};
 	unsigned int indices[] = { 
 		0, 1, 3,  // first Triangle
@@ -182,9 +196,16 @@ int main(void)
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	// Link Vertex Attributes pointers
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr); //*// check for GL_TRUE
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), nullptr); //*// check for GL_TRUE
 	glEnableVertexAttribArray(0);
 
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	// unbind VAO first, then VBO and EBO
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	/************************************************************************/
 	/*							RENDER LOOP									*/
@@ -198,15 +219,35 @@ int main(void)
 		// rendering commands:
 		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Gradually changing color via UNIFORM in shaders
+		/*float timeValue = (float) glfwGetTime();
+		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		glUseProgram(shaderProgram);
+		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 0.0f);*/
+
+
+		// Bind the vertex array in order to read
+		glBindVertexArray(VAO);
+		/*
+			bind VBO and EBO if we are going to change the attributes,
+			if only read action the only bind VAO
+		*/
+
 		// Draw a triangle
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
+
+
 		// check and call events and swap the buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+	// unbind buffers
+	glBindVertexArray(0);
 	// Clean all resources
 	glfwTerminate();
 	return 0;
